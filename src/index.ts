5d3ce9b52bb10f08b28bb1aa06477309390c8c3a
vVanish <chiriruho@gmail.com>
2022-04-18 14:58:41 -0400

    Add PORT extraction from env variables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { app } from "./server";
 import { AddressInfo } from "net";
+import { config } from "dotenv";
+
+config();
+type ENV = {
+  PORT?: number
+}
 
 // Should later be changed to automatically get the port provided by heroku in the environment
-const PORT = 5050;
+const { PORT = 5050 } = process.env as ENV;
 
 const httpServer = app.listen(PORT, () => console.log("Listening !!! @", (httpServer.address() as AddressInfo).port || "an unknown port"));
